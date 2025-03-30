cf03c4c65110d3537c884d0b208742fe18b5f0f8
HHKProgrammer <helin@DESKTOP-AE9GKSP>
2025-03-30 15:39:02 +0300

    added test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -1,7 +1,16 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
+/* Well this is where it gets interesting:
 
+	- Once the sell by date has passed, Quality degrades twice as fast
+	- The Quality of an item is never negative
+	- "Aged Brie" actually increases in Quality the older it gets
+	- The Quality of an item is never more than 50
+	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	Quality drops to 0 after the concert*/
 describe("Gilded Rose", () => {
   test("Normal item decreases in quality and sell inn each day", () => {
     const shop = new Shop([new Item("Normal Item", 10, 20)]);
@@ -24,5 +33,17 @@ describe("Gilded Rose", () => {
     expect(item.quality).to.equal(1);
   });
 
+  test("Sulfuras never decreases in quality or sellIn", () => {
+    const shop = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).to.equal(80);
+    expect(shop.items[0].sellIn).to.equal(0);
+  });
+
+  test("Backstage passes increase in quality as sellIn approaches", () => {
+    const shop = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]);
+    shop.updateQuality();
+    expect(shop.items[0].quality).to.equal(21);
+  });
 
 });
