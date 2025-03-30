1d0bb447c6b76bf717b61f0661413884563492d6
HHKProgrammer <helin@DESKTOP-AE9GKSP>
2025-03-30 15:18:09 +0300

    added test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -3,9 +3,12 @@ import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
-  test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
+  test("Normal item decreases in quality and sellIn each day", () => {
+    const shop = new Shop([new Item("Normal Item", 10, 20)]);
+    shop.updateQuality();
+    const [item] = shop.items;
+    expect(item.sellIn).to.equal(9);
+    expect(item.quality).to.equal(19);
   });
+
 });
